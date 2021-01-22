47290cdfb8b490f7e286b8c068c868858b793e05
Javier Daza <javierdazacomas@gmail.com>
2021-01-22 18:44:45 -0500

    Remuevo host y port del app.run

--- proyecto.py
+++ proyecto.py
@@ -19,7 +19,7 @@ app.register_blueprint(cargar)
 
 def main():
     database_proxy.create_tables([Estudiante], safe=True)
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
